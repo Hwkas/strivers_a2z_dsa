3c9484ec2d90fe6e93f4f4f232659a7815b45072
hwkas <amaansaifi999@gmail.com>
2025-05-21 22:27:09 +0530

    Check if an Array is Sorted

---
+++ array/easy/check_if_an_array_is_sorted.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+bool arraySortedOrNot(std::vector<int> &arr)
+{
+    int size = arr.size();
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        // For Ascending Order
+        if (arr[i] > arr[i + 1])
+        // For Descending Order
+        // if (arr[i] < arr[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    std::vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(50);
+
+    std::cout << "ans: " << (arraySortedOrNot(v) ? "True" : "False") << std::endl;
+    return 0;
+}
