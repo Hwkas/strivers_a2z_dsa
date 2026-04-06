275d2272da437bead2e6bc3e56551ceeb66553fa
Hwkas <amaansaifi999@gmail.com>
2026-04-06 23:41:25 +0530

    Finding Sqrt

---
+++ binary_search/answers/finding_sqrt.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int mySqrt(int x)
+{
+    int low = 0, high = x;
+    long long mid = 0;
+
+    while (low <= high)
+    {
+        mid = low + (high - low) / 2;
+
+        if ((mid * mid) <= x)
+        {
+
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return high;
+}
+
+int main()
+{
+    int x = 30;
+    std::cout << "Square root of " << x << " is: " << mySqrt(x) << std::endl;
+    return 0;
+}
